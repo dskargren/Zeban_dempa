b50ef9f6a6c76b754303f832f8815edbb0e9914e
Sebastian Sandberg <sebastian_sandberg@hotmail.com>
2013-09-18 10:12:00 +0200

    Upgraded the program for you, your welcome!

--- MrMisstag/MrMisstag/Program.cs
+++ MrMisstag/MrMisstag/Program.cs
@@ -8,17 +8,20 @@ using System.Threading.Tasks;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(fest)
     {
-        Console.WriteLine("Hej,\n Vi vill introducera dig för ett riktigt ostabilt program där du kan köpa din bil.");
-        string Ford;
-        string Volvo;
-        string Saab;
+        Console.WriteLine("Hej,\nVi vill introducera dig för ett riktigt ostabilt program där du kan köpa din bil.");
+        string Ford:
+        string Volvo:
+        string Saab:
 
 
         // La till lite i text
         Console.WriteLine("Skriv in vilken bil du vill köpa:");
 
+        Console.WriteLine("\n\n SUA!!");
+
+        Console.writeLine("Ditt program suger):
     }
 
 }
