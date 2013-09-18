aafa8837e37cdfb12c501202b3f7ac40381a0cd8
Sebastian Sandberg <sebastian_sandberg@hotmail.com>
2013-09-18 10:58:53 +0200

    Added an IF to the car names

--- MrMisstag/MrMisstag/Program.cs
+++ MrMisstag/MrMisstag/Program.cs
@@ -10,29 +10,47 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hej,\nVi vill introducera dig för ett riktigt ostabilt program där du kan köpa din bil.");
-       
-        string Ford;
-        string Volvo:
-        string Saab;
+        Console.WriteLine("Hej,\nVi vill introducera dig för ett riktigt bra program där du kan köpa din bil.");
+
+
+        
 
 
 
-
+        Console.WriteLine("\n");
 
 
         // La till lite i text
-        Console.WriteLine("Skriv in vilken bil du vill köpa:):
+        Console.WriteLine("Skriv in vilken bil du vill köpa");
+        Console.WriteLine("\n");
+        Console.WriteLine("Bilar att välja på: Volvo och Saab:");
+        Console.WriteLine("\n");
+        string inmatat = Console.ReadLine();
 
-        Console.WriteLine("Bilar att välja på: Ford, Volvo och Saab:"];
+        Console.WriteLine("Du har fått en " + inmatat + " Grattis!");
+        {
+            string Volvo = "\nVolvo: Volvo Orsakar Landets Värsta Olyckor!!!";
+            string Saab = "\nSaab: Sälj Aset Annars Byt!!!";
 
-        string x = Console.ReadLine();
+            if (inmatat == "Volvo" || inmatat == "volvo")
+            {
+                Console.WriteLine(Volvo);
+            }
+            else if (inmatat == "Saab" || inmatat == "saab")
+            {
+                Console.WriteLine(Saab);
+            }
+        }
 
-        Console.WriteLine("Du har köpt bilen" x "Grattis! ");
+        
 
 
 
-        Console.WriteLine("\n\n SUA!!");
+        Console.WriteLine("\n");
+
+
+
+        Console.WriteLine("\nSUA!!");
 
         Console.WriteLine("Ditt program suger");
 
